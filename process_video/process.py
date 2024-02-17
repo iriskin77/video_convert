6656a6a6a66bf1f7bdf7046d5d5c6bed61773487
iriskin77 <talcore@yandex.ru>
2024-02-17 22:11:18 +0500

    feat: log.json was added to approve that logging works

--- process_video/process.py
+++ process_video/process.py
@@ -5,7 +5,7 @@ import shutil
 from .models import VideoFile
 
 
-logger = logging.getLogger('main')
+logger = logging.getLogger('json_logger')
 
 
 def get_width_height_video(file_path: str) -> tuple[int, int]:
