761ca07445be9cf6d72fdeed6529de88225227cb
iriskin77 <talcore@yandex.ru>
2024-02-17 22:09:35 +0500

    fix: bug fixed (unused comma near field width)

--- process_video/models.py
+++ process_video/models.py
@@ -1,6 +1,7 @@
+import uuid
 from django.core.validators import MinValueValidator
 from django.db import models
-import uuid
+
 # Create your models here.
 
 
@@ -8,7 +9,7 @@ class VideoFile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     filename = models.CharField(max_length=255)
     file = models.FileField(upload_to="video_files")
-    width = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(20)]),
+    width = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(20)])
     height = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(20)])
     is_processing = models.BooleanField(default=None, null=True, blank=True)
     processing_success = models.BooleanField(default=None, null=True, blank=True)
